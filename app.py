fb635487b7eb6656179f932198eae40104848bb7
vedadri <vedadriyaddanapudi@gmail.com>
2025-06-15 11:42:14 +0530

    first commit

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from flask import Flask, render_template_string, send_file
-
-app = Flask(__name__)
-
-menu = {
-    'Tiffin 1': {'name': 'Idli with Sambar & Chutney', 'price': 40, 'image': 'idli.png'},
-    'Tiffin 2': {'name': 'Masala Dosa', 'price': 50, 'image': 'masala.png'},
-    'Tiffin 3': {'name': 'Poori', 'price': 40, 'image': 'poori.png'},
-    'Tiffin 4': {'name': 'Chapathi with Curry', 'price': 45, 'image': 'chapathi.png'},
-    'Lunch': {'name': 'Veg Meals', 'price': 130, 'image': 'image.png'}
-}
-
-def read_file(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        return f.read()
-
-@app.route('/')
-def welcome():
-    return render_template_string(read_file('welcome.html'))
-
-@app.route('/menu')
-def menu_page():
-    return render_template_string(read_file('index.html'), menu=menu)
-
-@app.route('/<filename>')
-def send_static_file(filename):
-    return send_file(filename)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+python-m venv env
+ cd env
+ CAScripts
+ Activate
+ TD install Django
+ PIP install Django
+ cd..
+ cd..
+ (a) Project creation
+ To Create a project:
+ django-admin start project projectname.
+ django-admin Start project project.
+ This will create a directory named project.
+ manage.py
+ myproject/--init--.py
+ settings.py
+ urls.py
+ wsgi.py
+ asgi.py
+  apps.py
+ models.py
+ tests.py
+ views.py
+ Set App1 in project/Settings.py
+ INSTALLED APPS=[----,----,
+ 'APP1',
+ ]
+ To create Admin
+ python manage.py make migrations
+ python manage py migrate
+ python manager.py crecite supeuser
+ username:----
+ email:----
+ password:---- (not visible)
+ confirm password:---- (Not visible)
+ Yes/No->Y
+ To run the development server:
+ Python manage.py run server
+ Then Access the admin interface with the following link
+ gives in the terminal
