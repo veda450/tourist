c815150277713ae9c1878d4629b25c4d86e0afd6
vedadri <vedadriyaddanapudi@gmail.com>
2025-06-16 21:02:17 +0530

    57

--- app.py
+++ app.py
@@ -1,40 +1,34 @@
-python-m venv env
- cd env
- CAScripts
- Activate
- TD install Django
- PIP install Django
- cd..
- cd..
- (a) Project creation
- To Create a project:
- django-admin start project projectname.
- django-admin Start project project.
- This will create a directory named project.
- manage.py
- myproject/--init--.py
- settings.py
- urls.py
- wsgi.py
- asgi.py
-  apps.py
- models.py
- tests.py
- views.py
- Set App1 in project/Settings.py
- INSTALLED APPS=[----,----,
- 'APP1',
- ]
- To create Admin
- python manage.py make migrations
- python manage py migrate
- python manager.py crecite supeuser
- username:----
- email:----
- password:---- (not visible)
- confirm password:---- (Not visible)
- Yes/No->Y
- To run the development server:
- Python manage.py run server
- Then Access the admin interface with the following link
- gives in the terminal
+from flask import Flask, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+DATABASE = 'hotels.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db:
+        db.close()
+
+@app.route('/')
+def index():
+    db = get_db()
+    hotels = db.execute('SELECT * FROM hotels').fetchall()
+    return render_template('index.html', hotels=hotels)
+
+@app.route('/hotel/<int:hotel_id>')
+def hotel(hotel_id):
+    db = get_db()
+    hotel = db.execute('SELECT * FROM hotels WHERE id = ?', (hotel_id,)).fetchone()
+    menu = db.execute('SELECT * FROM menu WHERE hotel_id = ?', (hotel_id,)).fetchall()
+    return render_template('hotel.html', hotel=hotel, menu=menu)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
